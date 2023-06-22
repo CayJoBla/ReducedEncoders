0fe4f016c97803fd4e02764e819639f149ccaebc
cayjobla <cayjobla@gmail.com>
2023-06-22 17:05:17 -0600

    Fix parameters in training script

--- train.sh
+++ train.sh
@@ -1,4 +1,3 @@
 python train_bert.py --model="cayjobla/bert-base-uncased-reduced" --dataset="cayjobla/wikipedia_tokenized" \
-    --num_shards=20 --index=$1 --batch_size=128 --finetune_base=False --num_epochs=1 --scheduler_type="linear" \
-    --num_warmup_steps=0 --logging_strat="step" --logging_steps=3000 --wandb_project="bert-base-uncased-reduced" \
-    --run_name="reduced-s$1"
+    --num_shards=20 --index=$1 --batch_size=128 --num_epochs=1 --scheduler_type="linear" --num_warmup_steps=0 \ 
+    --logging_strat="step" --logging_steps=3000 --wandb_project="bert-base-uncased-reduced" --run_name="reduced-s$1"
