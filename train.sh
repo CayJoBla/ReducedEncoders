03417412981b9513b83120deec4de89806666b53
cayjobla <cayjobla@gmail.com>
2023-06-22 17:10:01 -0600

    Fix script formatting

--- train.sh
+++ train.sh
@@ -1,3 +1,13 @@
-python train_bert.py --model="cayjobla/bert-base-uncased-reduced" --dataset="cayjobla/wikipedia_tokenized" \
-    --num_shards=20 --index=$1 --batch_size=128 --num_epochs=1 --scheduler_type="linear" --num_warmup_steps=0 \ 
-    --logging_strat="step" --logging_steps=3000 --wandb_project="bert-base-uncased-reduced" --run_name="reduced-s$1"
+python train_bert.py \
+    --model="cayjobla/bert-base-uncased-reduced" \
+    --dataset="cayjobla/wikipedia_tokenized" \
+    --num_shards=20 \
+    --index=$1 \
+    --batch_size=128 \
+    --num_epochs=1 \
+    --scheduler_type="linear" \
+    --num_warmup_steps=0 \
+    --logging_strat="step" \
+    --logging_steps=3000 \
+    --wandb_project="bert-base-uncased-reduced" \
+    --run_name="reduced-s$1" \
