a0c3c059c24591574ef690b949bece4b60bc5fa7
cayjobla <cayjobla@gmail.com>
2023-07-29 00:34:56 -0600

    Add data preprocessing for NSP/MLM

--- preprocess_data.py
+++ preprocess_data.py
@@ -1,141 +1,166 @@
 # preprocess_data.py
 
+from transformers import set_seed, AutoTokenizer
+from datasets import load_dataset, concatenate_datasets
+import random
+import math
 import argparse
-from transformers import AutoTokenizer, DataCollatorForLanguageModeling
-from datasets import load_dataset
-from huggingface_hub import get_full_repo_name
+import torch
 
+def preprocess(dataset=None, split=None, tokenizer=None, revision="main", shard_size=100_000,
+               train_size=None, test_size=0.1, seed=42, output_dir=None, verbose=False):
+    ## Set seed
+    set_seed(seed)
 
-def preprocess(dataset=None, split=None, tokenizer=None, train_size=None, test_size=0.15, chunk_size=64, 
-               mlm_probability=0.15, repo_name=None):
     ## Load tokenizer
-    print("Load tokenizer...")
+    if verbose: print("Load tokenizer...")
     if tokenizer is None: tokenizer = "bert-base-uncased"
-    tokenizer = AutoTokenizer.from_pretrained(tokenizer)
+    tokenizer = AutoTokenizer.from_pretrained(tokenizer, revision=revision)
 
     ## Load data and preprocess
-    # Define default values
     if dataset is None: dataset = "wikipedia"
     if split is None and dataset == "wikipedia": split = "20220301.en"
-    dataset_name = dataset.split("/")[-1]
 
-    print(f"Loading the {split} split of the {dataset} dataset...")
-    data = load_dataset(dataset) if split is None else load_dataset(dataset, split) # Load
-    data = data.shuffle(seed=42)            # Shuffle the dataset
+    if verbose: print(f"Loading the '{split}' split of the '{dataset}' dataset...")
+    data = load_dataset(dataset) if split is None else load_dataset(dataset, split)
 
-    ## Tokenize the data
-    def tokenize(batch):
-        return tokenizer(batch["text"])
+    ## Create bag of sentences
+    def get_bag(batch):
+       bag = [sentence.strip() 
+              for article in batch["text"] 
+              for paragraph in article.split("\n") 
+              for sentence in paragraph.split(".")    
+              if sentence.strip() != "" and paragraph.count(".") > 0]
+       return {"bag":bag}
 
-    print("Tokenize the dataset...")
-    tokenized_dataset = data.map(tokenize, batched=True, remove_columns=["id", "url", "title", "text"])
+    bag = data.map(
+        get_bag, 
+        batched=True,
+        remove_columns=['id','url','title','text'],
+        desc="Creating bag of sentences",
+    )["train"]["bag"]
+    bag_size = len(bag)
 
-    ## Chunk the data
-    def chunk_tokens(batch):
-        """Concatenate the samples in the batch and split into chunks of equal length"""
-        concatenated_batch = {k: sum(batch[k], []) for k in batch.keys()}  # Concatenate
-        total_length = len(concatenated_batch[list(batch.keys())[0]])  # Get total length
-        total_length = (total_length // chunk_size) * chunk_size  # Drop the last chunk if smaller
-        result = {
-            k: [t[i:i+chunk_size] for i in range(0, total_length, chunk_size)]
-            for k, t in concatenated_batch.items()
-        }
-        # Create a new labels column
-        result["labels"] = result["input_ids"].copy()
-        return result
+    def preprocess(batch):
+        random.seed(seed)
+        sentence1 = []
+        sentence2 = []
+        labels = []
 
-    print("Chunk the data...")
-    lm_dataset = tokenized_dataset.map(chunk_tokens, batched=True)
+        for article in batch["text"]:
+            for paragraph in article.split("\n"):
+                sentences = [sentence.strip() for sentence in paragraph.split(".") if sentence.strip() != ""]
+                num_sentences = len(sentences)
+                if num_sentences > 1:
+                    start = random.randint(0, num_sentences-2)
+                    sentence1.append(sentences[start])
+                    if random.random() < 0.5:
+                        sentence2.append(sentences[start+1])
+                        labels.append(0)
+                    else:
+                        sentence2.append(bag[random.randint(0, bag_size-1)])
+                        labels.append(1)
+        
+        inputs = tokenizer(sentence1, sentence2, return_tensors="pt", max_length=512, 
+                           truncation=True, padding="max_length")
+        inputs["next_sentence_label"] = torch.LongTensor(labels)
+        inputs["labels"] = inputs.input_ids.detach().clone()
+        return inputs
 
-    # Shuffle and split into train-test sets
-    print("Split the data into train and test sets...")
-    lm_dataset = lm_dataset["train"].train_test_split(train_size=train_size, 
-                                                      test_size=test_size, 
-                                                      seed=42)  
+    def sharded_preprocess(dataset, shard_size=100_000):
+        num_shards = math.ceil(len(dataset["train"]) / shard_size)
+        shards = []
+        for i in range(num_shards):
+            if i == num_shards-1:
+                data_shard = dataset["train"]
+            else:
+                dataset = dataset["train"].train_test_split(test_size=min(shard_size, len(dataset["train"])), shuffle=False)
+                data_shard = dataset["test"]
+            shards.append(data_shard.map(
+                preprocess, 
+                batched=True, 
+                remove_columns=['id', 'url', 'title', 'text'], 
+                desc=f"Preprocess shard {i+1} of the dataset for NSP"
+            ))
+        return concatenate_datasets(shards, split="train")
 
-    # Mask the test data once to reduce randomness at each evaluation
-    def insert_random_mask(batch):
-        data_collator = DataCollatorForLanguageModeling(tokenizer, mlm_probability=mlm_probability, return_tensors="pt")
-        features = [dict(zip(batch, t)) for t in zip(*batch.values())]
-        masked_inputs = data_collator(features)
-        return {"masked_" + k: v for k, v in masked_inputs.items()}
+    ## Preprocess the data
+    if verbose: print("Preprocess the data...")
+    processed_data = sharded_preprocess(data, shard_size=shard_size)
 
-    print("Add random mask to test set...")
-    column_names = lm_dataset['test'].column_names
-    eval_dataset = lm_dataset['test'].map(insert_random_mask, batched=True, remove_columns=column_names)
-    eval_dataset = eval_dataset.rename_columns({"masked_" + column: column for column in column_names})
+    ## Split into train/test sets
+    if verbose: print("Split data into train and test sets")
+    input_data = processed_data.train_test_split(train_size=train_size, test_size=test_size, seed=seed)
 
-    # Cast the features to match the train dataset
-    new_features = lm_dataset['train'].features.copy()
-    eval_dataset = eval_dataset.cast(new_features)
-
-    lm_dataset['test'] = eval_dataset
-    
-    print("Push dataset to hub...")
-    if repo_name is None:
-        repo_name = f"{dataset_name}-tokenized"
-    repo_name = get_full_repo_name(repo_name)
-    lm_dataset.push_to_hub(repo_name)
+    ## Save dataset to disk
+    if output_dir is None:
+        output_dir = f"{dataset}-pretrain-processed"
+    input_data.save_to_disk(output_dir)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Tokenize, chunk, and split the dataset. Randomly mask the test data. Push the dataset to the HuggingFace hub.")
     parser.add_argument(
         '--dataset',
-        '-d',
         help=("The dataset to preprocess. Default is the wikipedia dataset."),
         type=str,
         default=None
     )
     parser.add_argument(
         '--split',
-        '-s',
         help=("The split of the dataset to use. Default is the '20220301.en' split of the wikipedia dataset."),
         type=str,
         default=None
     )
     parser.add_argument(
         '--tokenizer',
-        '-t',
         help=("The tokenizer to use for preprocessing. Default is 'bert-base-uncased'."),
         type=str,
         default=None
     )
     parser.add_argument(
+        '--revision',
+        help=("The revision of the tokenizer to use. Default is 'main'"),
+        type=str,
+        default="main"
+    )
+    parser.add_argument(
+        '--shard_size',
+        help=("The size of shard to use in data preprocessing. Larger sizes run faster, smaller sizes cache more often. Default is 100_000."),
+        type=int,
+        default=100_000
+    )
+    parser.add_argument(
         '--train_size',
-        '-a',
         help=("The size of the training set when splitting the data. Default is None."),
         type=float,
         default=None
     )
     parser.add_argument(
         '--test_size',
-        '-b',
-        help=("The size of the test set when splitting the data. Default is 0.15."),
+        help=("The size of the test set when splitting the data. Default is 0.1."),
         type=float,
         default=0.15
     )
     parser.add_argument(
-        '--chunk_size',
-        '-c',
-        help=("The number of tokens to include in each data chunk. Default is 64"),
+        '--seed',
+        help=("The seed to use for randomness in preprocessing. Default is 42."),
         type=int,
-        default=64
-    )
-    parser.add_argument(
-        '--mlm_probability',
-        '-p',
-        help=("The probability to use when applying masking to the test set. Default is 0.15."),
-        type=float,
-        default=0.15
-    )
-    parser.add_argument(
-        '--repo_name',
-        '-r',
-        help=("The HuggingFace repository to push the dataset to. Default is '{dataset_name}-tokenized'."),
-        type=str,
         default=None
     )
+    parser.add_argument(
+        '--output_dir',
+        help=("The output directory to save the preprocessed data to. If None, a default directory is chosen."),
+        type=str,
+        default=None
+    )   
+    parser.add_argument(
+        '--verbose',
+        '-v',
+        help=("Whether to print preprocessing progress information. Default is False."),
+        default=False,
+        action="store_true"
+    )    
 
     kwargs = parser.parse_args()
 
