2c55da5264b2017d36fc2f6f576e79b3fcffb29d
cayjobla <cayjobla@gmail.com>
2024-03-04 12:31:16 -0700

    Create a ReducedConfig mixin class to inherit from

--- reduced_encoders/models/bert_reduced/configuration_bert_reduced.py
+++ reduced_encoders/models/bert_reduced/configuration_bert_reduced.py
@@ -1,27 +1,12 @@
 # configuration_bert_reduced.py
 
 from transformers import BertConfig, AutoConfig
+from ...configuration_reduced import ReducedConfig
 
-class BertReducedConfig(BertConfig):
+class BertReducedConfig(ReducedConfig, BertConfig):
     """This class wraps the BertConfig class to add the dimensionality reduction parameters."""
 
     model_type = "bert_reduced"
 
-    def __init__(self, *args, reduction_sizes=(48,), **kwargs):
+    def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.reduction_sizes = reduction_sizes
-        self.reduced_size = reduction_sizes[-1]
-
-    @classmethod
-    def from_config(cls, config, **kwargs):
-        if isinstance(config, BertConfig):
-            config = cls(**config.__dict__, **kwargs)
-        else:
-            raise ValueError("Parameter config should be an instance of class `BertConfig`.")
-        config.model_type = cls.model_type
-        return config
-
-    @classmethod
-    def from_pretrained(cls, pretrained_model_name_or_path, **kwargs):
-        config = AutoConfig.from_pretrained(pretrained_model_name_or_path, **kwargs)
-        return cls.from_config(config)
--- reduced_encoders/models/mpnet_reduced/configuration_mpnet_reduced.py
+++ reduced_encoders/models/mpnet_reduced/configuration_mpnet_reduced.py
@@ -1,28 +1,13 @@
 # configuration_mpnet_reduced.py
 
 from transformers import MPNetConfig, AutoConfig
+from ...configuration_reduced import ReducedConfig
 
-class MPNetReducedConfig(MPNetConfig):
+class MPNetReducedConfig(ReducedConfig, MPNetConfig):
     """This class wraps the MPNetConfig class to add the dimensionality reduction parameters and the pooling mode"""
 
     model_type = "mpnet_reduced"
 
-    def __init__(self, *args, reduction_sizes=(48,), pooling_mode="mean", **kwargs):
+    def __init__(self, *args, pooling_mode="mean", **kwargs):
         super().__init__(*args, **kwargs)
-        self.reduction_sizes = reduction_sizes
-        self.reduced_size = reduction_sizes[-1]
         self.pooling_mode = pooling_mode
-
-    @classmethod
-    def from_config(cls, config, **kwargs):
-        if isinstance(config, MPNetConfig):
-            config = cls(**config.__dict__, **kwargs)
-        else:
-            raise ValueError("Parameter config should be an instance of class `MPNetConfig`.")
-        config.model_type = cls.model_type
-        return config
-
-    @classmethod
-    def from_pretrained(cls, pretrained_model_name_or_path, **kwargs):
-        config = AutoConfig.from_pretrained(pretrained_model_name_or_path, **kwargs)
-        return cls.from_config(config)
