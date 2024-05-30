6becdda97f25df23a2623ca13a78db558507aaaf
cayjobla <cayjobla@gmail.com>
2024-05-30 14:54:41 -0600

    Do some basic evaluation on the variable compression model

--- evaluate_compressed.sh
+++ evaluate_compressed.sh
@@ -1,18 +1,18 @@
 export WANDB_PROJECT=mpnet-compressed-glue
-export CUDA_VISIBLE_DEVICES="0,1,2"
+export CUDA_VISIBLE_DEVICES="1"
 export TASK_NAME=$1
 
 python evaluate_compressed.py \
   --model cayjobla/all-mpnet-base-v2-compressed \
   --tokenizer cayjobla/all-mpnet-base-v2-compressed \
-  --revision main \
+  --revision variable \
   --task $TASK_NAME \
   --batch_size 16 \
   --learning_rate 3e-5 \
   --num_epochs 3 \
   --logging_steps 20 \
   --output_dir all-mpnet-base-v2-compressed \
-  --run_name glue-$TASK_NAME \
+  --run_name variable-glue-$TASK_NAME \
   --seed 916 \
   -v
 
