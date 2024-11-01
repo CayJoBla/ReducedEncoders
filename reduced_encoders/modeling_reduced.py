82fdf61d4fa8f29e5f4d16f123c2b83f131046ae
cayjobla <cayjobla@gmail.com>
2024-11-01 12:36:22 -0600

    Small bug fix with forward pass

--- reduced_encoders/modeling_reduced.py
+++ reduced_encoders/modeling_reduced.py
@@ -28,9 +28,9 @@ class DimReshape(nn.Module):
         self.dropout = nn.Dropout(config.hidden_dropout_prob)
         
     def forward(self, x):
-        output = self.activation(output)
+        output = self.activation(x)
         output = self.dropout(output)
-        output = self.linear(x)
+        output = self.linear(output)
         return output
 
 
