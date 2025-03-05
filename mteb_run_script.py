cf96097095252de75793491b5612740025c24073
cayjobla <cayjobla@gmail.com>
2025-03-05 15:57:37 -0700

    Update to MTEB(eng, v2) instead of v1

--- mteb_run_script.py
+++ mteb_run_script.py
@@ -4,6 +4,6 @@ from sentence_transformers import SentenceTransformer
 model_name = "sentence-transformers/all-mpnet-base-v2"
 model = SentenceTransformer(model_name)
 
-benchmark = mteb.get_benchmark("MTEB(eng, v1)")
+benchmark = mteb.get_benchmark("MTEB(eng, v2)")
 evaluation = mteb.MTEB(tasks=benchmark)
 results = evaluation.run(model, output_folder=f"results/{model_name}")
